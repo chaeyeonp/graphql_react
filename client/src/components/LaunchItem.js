d7dd1bdf793ea207e3a6c4acd07044bdc188c312
chaeyeonp <chaeyeon.park@outlook.com>
2020-11-25 13:56:11 +0900

    Complete

--- client/src/components/LaunchItem.js
+++ client/src/components/LaunchItem.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import classNames from 'classnames';
+import Moment from 'react-moment';
+import {Link} from 'react-router-dom';
 
 export default function LaunchItem({launch:{flight_number, mission_name, launch_date_local,launch_success}}){
     return(
         <div className="card card-body mb-3">
             <div className="row">
                 <div className="col-md-9">
-                    <h4>Mission: {mission_name}</h4>
-                    <h4>Date: {launch_date_local}</h4>
+                    <h4>Mission: <span className={classNames({
+                        'text-success':launch_success,
+                        'text-danger': !launch_success
+                    })}>{mission_name}</span></h4>
+                    <h4>Date: <Moment format = "YYYY-MM-DD HH:mm">{launch_date_local}</Moment></h4>
 
 
                 </div>
                 <div className="col-md-3">
-                    <button className = "btn btn-secondary">
+                    <Link to={`/launch/${flight_number}`} className = "btn btn-secondary">
                         Launch Details
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
