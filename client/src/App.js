33db02ccb2f8aa2e3d8727d9be9a69bfb11fde9b
chaeyeonp <chaeyeon.park@outlook.com>
2020-12-09 09:07:55 +0900

    change Launches.js to react-hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {ApolloProvider} from 'react-apollo';
+import {ApolloProvider as ApolloHooksProvider} from 'react-apollo-hooks';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Launches from './components/Launches';
 import Launch from './components/Launch';
 import './App.css';
@@ -15,17 +16,19 @@ function App() {
 
     return (
         <ApolloProvider client={client}>
-            <Router>
-                <div className="container">
-                    <img
-                        src={logo}
-                        alt="SpaceX"
-                        style={{ width: 300, display: 'block', margin: 'auto' }}
-                    />
-                    <Route exact path="/" component={Launches} />
-                    <Route exact path="/launch/:flight_number" component={Launch} />
-                </div>
-            </Router>
+            <ApolloHooksProvider client={client}>
+                <Router>
+                    <div className="container">
+                        <img
+                            src={logo}
+                            alt="SpaceX"
+                            style={{width: 300, display: 'block', margin: 'auto'}}
+                        />
+                        <Route exact path="/" component={Launches}/>
+                        <Route exact path="/launch/:flight_number" component={Launch}/>
+                    </div>
+                </Router>
+            </ApolloHooksProvider>
         </ApolloProvider>
     );
 }
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,8 +1,10 @@
-import React, {Component, Fragment} from 'react';
+import React, {Component, Fragment, Suspense} from 'react';
 import gql from 'graphql-tag';
 import {Query} from 'react-apollo';
 import LaunchItem from './LaunchItem';
 import MissionKey from "./MissionKey";
+import {useQuery} from '@apollo/react-hooks';
+import {render} from "@testing-library/react";
 
 
 const LAUNCHES_QUERY = gql`
@@ -16,35 +18,30 @@ query LaunchesQuery{
 }
 `;
 
-export class Launches extends Component {
-    render() {
-        return (
-            <Fragment>
-                <h1 className="display-4 my-3">Launches</h1>
-                <MissionKey/>
-                <Query query={LAUNCHES_QUERY}>
-                    {
-                        ({loading, error, data}) => {
-                            if (loading) return <h4>Loading...</h4>
-                            if (error) console.log(error);
+function Launches(props) {
+    const {data, error, loading} = useQuery(LAUNCHES_QUERY);
 
-                            return <Fragment>
-                                {
-                                    data.launches.map(launch=>(
-                                        <LaunchItem key = {launch.flight_number} launch={launch}/>
-                                    ))
-                                }
+    if (loading) return <div>Loading</div>;
 
-                            </Fragment>
+    if (error) return <div>Error! {error.message}</div>;
 
-                            return <h1>test</h1>;
-                        }
-                    }
-                </Query>
-            </Fragment>
 
-        )
-    }
+    return (
+
+        <>
+            <h1 className="display-4 my-3">{"Launches"}</h1>
+            <MissionKey/>
+
+            {
+                data.launches.map(launch => (
+                    <LaunchItem key={launch.flight_number} launch={launch}/>
+                ))
+            }
+
+        </>
+
+    );
 }
 
-export default Launches
+
+export default Launches;
